120ff7d18a92840846ceca61c0ea85e98d838b32
ducquyet1711 <gunnynhoc7@gmail.com>
2023-06-19 12:17:11 +0700

    init project

---
+++ src/main/java/net/codejava/product/Product.java
@@ -0,0 +1,68 @@
+package net.codejava.product;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+	@Column(name = "brand")
+	private String brand;
+	@Column(name = "madein")
+	private String madein;
+	@Column(name = "price")
+	private float price;
+
+	public Product() {
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getMadein() {
+		return madein;
+	}
+
+	public void setMadein(String madein) {
+		this.madein = madein;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+}
